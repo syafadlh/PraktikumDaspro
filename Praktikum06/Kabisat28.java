5a0f2cf7a5f183b10f9a05069556526158412aca
syafadlh <adilahsyafaa@gmail.com>
2023-10-11 14:51:42 +0700

    modif 3 segitiga

--- Praktikum06/Kabisat28.java
+++ Praktikum06/Kabisat28.java
@@ -7,7 +7,7 @@ public class Kabisat28 {
 
         System.out.print("masukkan tahun : ");
         tahun = sc.nextInt();
-        if((tahun % 4) == 0 && (tahun % 100) != 0){
+        if((tahun % 4) == 0 && (tahun % 100) != 0 || (tahun % 400) ==0){
             System.out.println("Tahun Kabisat");
         }else{
             System.out.println("Bukan Tahun Kabisat");
