be41f8e77749d0044ccacb3a8e27cf2e41dfbe0a
syafadlh <adilahsyafaa@gmail.com>
2023-10-18 14:51:36 +0700

    Modif For Kelipatan

--- Praktikum07/ForKelipatan28.java
+++ Praktikum07/ForKelipatan28.java
@@ -9,12 +9,14 @@ public class ForKelipatan28 {
         System.out.print("Masukkan bilangan 1-9: ");
         bilangan = sc.nextInt();
 
-        for (int i = 0; i < 50; ++i) {
+        for (int i = 0; i < 50; i++) {
             if (i % bilangan == 0) {
                 total += i;
-                ++counter;
+                counter++;
             }
         }
+        double average = (double) total / counter;
+        System.out.println("Rata-rata : " + average);
         System.out.printf("Pada retang 1-50 tersebut %d bilangan kelipatan %d dengan total %d", counter, bilangan, total);
     }
 }
