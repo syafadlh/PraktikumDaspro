a0ffc6c7fe991ba434aa18f8ea736420ecef7f8e
syafadlh <adilahsyafaa@gmail.com>
2023-11-01 11:25:39 +0700

    Array Nilai

---
+++ Praktikum09/ArrayNilai28.java
@@ -0,0 +1,15 @@
+package Praktikum09;
+import java.util.Scanner;
+public class ArrayNilai28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int [] nilaiAkhir = new int [10];
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Masukkan nilai akhir mahasiswa ke-" + (i+1) + ": ");
+            nilaiAkhir[i] = sc.nextInt();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Nilai akhir mahasiswa ke-" + (i+1) + " adalah " + nilaiAkhir[i] );
+        }
+    }   
+}
