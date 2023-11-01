fb8a2958838d337e518c6b436a9eb43a1fd29428
syafadlh <adilahsyafaa@gmail.com>
2023-11-01 14:12:19 +0700

    Modifikasi Array Nilai

--- Praktikum09/ArrayNilai28.java
+++ Praktikum09/ArrayNilai28.java
@@ -4,17 +4,34 @@ public class ArrayNilai28 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         int [] nilaiAkhir = new int [10];
+        int max = nilaiAkhir[0], min;
+        float nilaiRata, total = 0;
 
         for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir mahasiswa ke-" + (i+1) + ": ");
             nilaiAkhir[i] = sc.nextInt();
+            total = total + nilaiAkhir [i];
+
+            if (nilaiAkhir[i] > max) {
+                max = nilaiAkhir[i];
+            }
+        }
+        min = nilaiAkhir[0];
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] < min) {
+                min = nilaiAkhir[i];   
+            }   
         }
         for (int i = 0; i < 10; i++) {
             if (nilaiAkhir[i] > 70) {
                 System.out.println("Nilai akhir mahasiswa ke-" + (i+1) + " lulus ");
             } else {
                 System.out.println("Nilai akhir mahasiswa ke-" + (i+1) + " tidak lulus ");
-            }
+            }  
         }
+        nilaiRata = total/10;
+        System.out.println("Hasil nilai rata-rata adalah: " + nilaiRata);
+        System.out.println("Hasil nilai tertinggi adalah: " + max);
+        System.out.println("Hasil nilai terendah adalah: " + min);
     }   
 }
