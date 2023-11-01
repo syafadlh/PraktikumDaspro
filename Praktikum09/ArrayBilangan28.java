ca370a94bffbb56d4daf5dcaf161e8cab0816fb8
syafadlh <adilahsyafaa@gmail.com>
2023-11-01 11:13:15 +0700

    Modifikasi Array Bilangan

--- Praktikum09/ArrayBilangan28.java
+++ Praktikum09/ArrayBilangan28.java
@@ -2,15 +2,14 @@ package Praktikum09;
 import java.util.Scanner;
 public class ArrayBilangan28 {
     public static void main(String[] args) {
-        int [] bil = new int [4];
-        bil [0] = 5;
-        bil [1] = 13;
-        bil [2] = -7;
-        bil [3] = 17;
+        double [] bil = new double [4];
+        bil [0] = 5.0;
+        bil [1] = 12867;
+        bil [2] = 7.5;
+        bil [3] = 2000000;
 
-        System.out.println(bil [0]);
-        System.out.println(bil [1]);
-        System.out.println(bil [2]);
-        System.out.println(bil [3]);
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(bil [i]);
+        }
     }
 }
