35c3dbb0907acfbe8971cbf038d87975a66b0cb0
syafadlh <adilahsyafaa@gmail.com>
2023-11-01 14:59:32 +0700

    Modifikasi Linear Search

--- Praktikum09/LinearSearch28.java
+++ Praktikum09/LinearSearch28.java
@@ -2,16 +2,31 @@ package Praktikum09;
 import java.util.Scanner;
 public class LinearSearch28 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
         int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
-        int hasil = 0;
+        int key, jumlahElemen;
+        int hasil = -1;
+
+        System.out.print("Masukkan jumlah elemen array: ");
+        jumlahElemen = sc.nextInt();
 
         for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukkan elemen array ke-" + i + ": ");
+            arrayInt[i] = sc.nextInt();
+        }
+        System.out.print("Masukkan key yang ingin dicari: ");
+        key = sc.nextInt();
+
+        for (int i = 0; i < jumlahElemen; i++) {
             if (arrayInt[i] == key) {
                 hasil = i;
                 break;
             }
         }
-        System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
+        if (hasil != -1) {
+            System.out.print("Key ada di posisi indeks ke- " + hasil); 
+        } else {
+            System.out.print("Key tidak ditemukan");
+        }
     }
 }
