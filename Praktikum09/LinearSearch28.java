bdf15c5d4a33e171c0c00b75e551602aba80ee5f
syafadlh <adilahsyafaa@gmail.com>
2023-11-02 13:15:55 +0700

    Modifikasi 2 Linear Search

--- Praktikum09/LinearSearch28.java
+++ Praktikum09/LinearSearch28.java
@@ -2,31 +2,39 @@ package Praktikum09;
 import java.util.Scanner;
 public class LinearSearch28 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
         int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key, jumlahElemen;
-        int hasil = -1;
+        int banyakElemen;
+        int[] hasilArray = new int[5];
+        int jumlahHasil = 0;
 
-        System.out.print("Masukkan jumlah elemen array: ");
-        jumlahElemen = sc.nextInt();
+        System.out.print("Masukkan banyaknya elemen array: ");
+        banyakElemen = sc.nextInt();
 
         for (int i = 0; i < arrayInt.length; i++) {
             System.out.print("Masukkan elemen array ke-" + i + ": ");
             arrayInt[i] = sc.nextInt();
         }
         System.out.print("Masukkan key yang ingin dicari: ");
-        key = sc.nextInt();
+        int key = sc.nextInt();
 
-        for (int i = 0; i < jumlahElemen; i++) {
+        for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
-                hasil = i;
-                break;
+                hasilArray[jumlahHasil] = i;
+                jumlahHasil++;
             }
         }
-        if (hasil != -1) {
-            System.out.print("Key ada di posisi indeks ke- " + hasil); 
+        if (jumlahHasil > 0) {
+            System.out.print("Key ditemukan di posisi indeks: ");
+            for (int i = 0; i < jumlahHasil; i++) {
+                System.out.print(hasilArray[i]);
+                if (i < jumlahHasil - 1) {
+                    System.out.print(", ");
+                }
+            }
+            System.out.println();
         } else {
-            System.out.print("Key tidak ditemukan");
+            System.out.println("Key tidak ditemukan dalam array.");
         }
     }
 }
