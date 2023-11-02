2d03c0cf9b9874d5e8611f6856f998492e0fe0ee
syafadlh <adilahsyafaa@gmail.com>
2023-11-02 12:07:50 +0700

    Modifikasi 2 Array Nilai

--- Praktikum09/ArrayNilai28.java
+++ Praktikum09/ArrayNilai28.java
@@ -4,13 +4,15 @@ public class ArrayNilai28 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         int [] nilaiAkhir = new int [10];
-        int max = nilaiAkhir[0], min;
+        int max = nilaiAkhir[0];
+        int min;
         float nilaiRata, total = 0;
+        int jumlahLulus = 0, jumlahTidakLulus = 0;
 
         for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir mahasiswa ke-" + (i+1) + ": ");
             nilaiAkhir[i] = sc.nextInt();
-            total = total + nilaiAkhir [i];
+            total += nilaiAkhir [i];
 
             if (nilaiAkhir[i] > max) {
                 max = nilaiAkhir[i];
@@ -20,7 +22,12 @@ public class ArrayNilai28 {
         for (int i = 0; i < nilaiAkhir.length; i++) {
             if (nilaiAkhir[i] < min) {
                 min = nilaiAkhir[i];   
-            }   
+            }
+            if (nilaiAkhir[i] > 70) {
+                jumlahLulus++;
+            } else {
+                jumlahTidakLulus++;
+            }
         }
         for (int i = 0; i < 10; i++) {
             if (nilaiAkhir[i] > 70) {
@@ -33,5 +40,7 @@ public class ArrayNilai28 {
         System.out.println("Hasil nilai rata-rata adalah: " + nilaiRata);
         System.out.println("Hasil nilai tertinggi adalah: " + max);
         System.out.println("Hasil nilai terendah adalah: " + min);
+        System.out.println("Jumlah mahsaiswa yang lulus: " + jumlahLulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + jumlahTidakLulus);
     }   
 }
