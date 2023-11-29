03067a92cabe64ff119be3f19271b086e12a011f
syafadlh <adilahsyafaa@gmail.com>
2023-11-29 15:03:52 +0700

    School

--- Praktikum13/School28.java
+++ Praktikum13/School28.java
@@ -10,38 +10,6 @@ public class School28 {
         display(students1B);
         display(students1C);
         display(students1D);
-        
-        // System.out.printf("=========================%n");
-        // System.out.printf("| %-3s | %-15s |%n", "NO", "NAME");
-        // System.out.printf("=========================%n");
-        // for (int i = 0; i < students1A.length; i++) {
-        //     System.out.printf("| %-3d | %-15s |%n", (i+1), students1A[i]);
-        // }
-        // System.out.printf("=========================%n%n");
-
-        // System.out.printf("=========================%n");
-        // System.out.printf("| %-3s | %-15s |%n", "NO", "NAME");
-        // System.out.printf("=========================%n");
-        // for (int i = 0; i < students1B.length; i++) {
-        //     System.out.printf("| %-3d | %-15s |%n", (i+1), students1B[i]);
-        //  }
-        // System.out.printf("=========================%n%n");
-
-        // System.out.printf("=========================%n");
-        // System.out.printf("| %-3s | %-15s |%n", "NO", "NAME");
-        // System.out.printf("=========================%n");
-        // for (int i = 0; i < students1C.length; i++) {
-        // System.out.printf("| %-3d | %-15s |%n", (i+1), students1C[i]);
-        // }
-        // System.out.printf("=========================%n%n");
-
-        // System.out.printf("=========================%n");
-        // System.out.printf("| %-3s | %-15s |%n", "NO", "NAME");
-        // System.out.printf("=========================%n");
-        // for (int i = 0; i < students1D.length; i++) {
-        // System.out.printf("| %-3d | %-15s |%n", (i+1), students1D[i]);
-        // }
-        // System.out.printf("=========================%n%n");
     }
     public static void display(String[] students) {
         System.out.printf("+++++++++++++++++++++++++%n");
