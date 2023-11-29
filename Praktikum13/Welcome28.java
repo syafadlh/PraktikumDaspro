bb8abdd84709a54b32e79d6780bc70619274367a
syafadlh <adilahsyafaa@gmail.com>
2023-11-29 10:56:44 +0700

    Welcome

---
+++ Praktikum13/Welcome28.java
@@ -0,0 +1,11 @@
+package Praktikum13;
+
+public class Welcome28 {
+    
+    public static void displayWelcomeMessage() {
+        System.out.println("Selamat datang di Politeknik Negeri Malang");
+    }
+    public static void main(String[] args) {
+        displayWelcomeMessage();
+    }
+}
